1fea4501d92b86ab664279947ad9c5b0b479b61d
Alex Sherbina <alex.sherbina.work@gmail.com>
2024-09-06 12:27:16 +0300

    added variables

--- src/PanelGame/GameModeSelector/GameModeSelector.tsx
+++ src/PanelGame/GameModeSelector/GameModeSelector.tsx
@@ -1,36 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./GameModeSelector.scss";
 
+const HUMAN_VS_AI = "human-ai";
+const HUMAN_VS_HUMAN = "human-human";
+
+type values = "human-human" | "human-ai";
 interface GameMode {
-  value: "human-human" | "human-ai";
+  value: values;
   label: string;
 }
 
 const gameModes: GameMode[] = [
-  { value: "human-human", label: "Human vs. Human" },
-  { value: "human-ai", label: "Human vs. AI" },
+  { value: HUMAN_VS_HUMAN, label: "Human vs. Human" },
+  { value: HUMAN_VS_AI, label: "Human vs. AI" },
 ];
 
 interface GameModeSelectorTypes {
   computerPlayer?: boolean;
   setComputerPlayer: (val: boolean) => void;
+  gameModes?: [];
 }
 
 const GameModeSelector: React.FC<GameModeSelectorTypes> = ({
+  computerPlayer,
   setComputerPlayer,
 }) => {
   const [selectedMode, setSelectedMode] =
-    useState<GameMode["value"]>("human-human");
+    useState<GameMode["value"]>(HUMAN_VS_HUMAN);
 
   const handleModeChange = (value: GameMode["value"]) => {
     setSelectedMode(value);
-    if (value === "human-ai") {
+    if (value === HUMAN_VS_AI) {
       setComputerPlayer(true);
     }
-    if (value === "human-human") {
+    if (value === HUMAN_VS_HUMAN) {
       setComputerPlayer(false);
     }
   };
+
+  useEffect(() => {
+    if (computerPlayer) {
+      setSelectedMode(HUMAN_VS_AI);
+    } else {
+      setSelectedMode(HUMAN_VS_HUMAN);
+    }
+  }, [computerPlayer]);
 
   return (
     <div className="game-mode-selector">
